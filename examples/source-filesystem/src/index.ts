ceaa1d7e61f822812af153c013e6b243d0aa5d4b
Alexander Snov <snov.contact@gmail.com>
2020-09-25 01:11:21 +0700

    upgrade @istok/firebase-source

--- examples/source-filesystem/src/index.ts
+++ examples/source-filesystem/src/index.ts
@@ -4,6 +4,7 @@ import { createFilesystemSource } from '@istok/source-filesystem';
 export async function main() {
   const source = createFilesystemSource({
     root: path.resolve(process.cwd(), './data'),
+    exclude: '__meta__',
   });
 
   const resources = await source.getList();
